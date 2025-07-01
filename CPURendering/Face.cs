4a91dc14455f1a3324f8869a859c264e1fee369a
Sam Hatami <mhatami@hotmail.com>
2025-07-01 06:20:00 +0200

    Mesh and face

---
+++ CPURendering/Face.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace CPURendering;
+
+public struct Face
+{
+    public int A;
+    public int B;
+    public int C;
+    public int UvA;
+    public int UvB;
+    public int UvC;
+    Vector3 Normal;
+
+    public Face(int a, int b, int c, Vector3 normal)
+    {
+        A = a;
+        B = b;
+        C = c;
+        Normal = normal;
+    }
+}
---
+++ CPURendering/Mesh.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace CPURendering;
+
+public struct Mesh
+{
+    public Vector3[] Vertices;
+    public Face[] Faces;
+    public Vector3 Rotation;
+}
