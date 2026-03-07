5e9e02e17ade155085241245d920721cb79cc5d9
Sam Hatami <mhatami@hotmail.com>
2026-03-07 13:49:36 +0100

    Revert "Load truss meshes synchronously to avoid delayed member appearance"
    This reverts commit a35694fe32bd13053b25721f7359ffd49137ff57.

--- SamLabs.Gfx.Engine/Blueprints/Truss/FrameTowerBlueprint.cs
+++ SamLabs.Gfx.Engine/Blueprints/Truss/FrameTowerBlueprint.cs
@@ -27,11 +27,11 @@ public class FrameTowerBlueprint : EntityBlueprint
 
     public override string Name => EntityNames.FrameTower;
 
-    public override void Build(Entity entity, MeshDataComponent meshData = default)
+    public override async void Build(Entity entity, MeshDataComponent meshData = default)
     {
         entity.Type = EntityType.SceneObject;
 
-        _memberBlueprint.EnsureMeshesLoaded();
+        await _memberBlueprint.EnsureMeshesLoaded();
 
         _componentRegistry.SetComponentToEntity(new TransformComponent
         {
--- SamLabs.Gfx.Engine/Blueprints/Truss/MemberElementBlueprint.cs
+++ SamLabs.Gfx.Engine/Blueprints/Truss/MemberElementBlueprint.cs
@@ -33,19 +33,19 @@ public class MemberElementBlueprint : EntityBlueprint
 
     public override string Name { get; } = EntityNames.MemberElement;
 
-    public void EnsureMeshesLoaded()
+    public async Task EnsureMeshesLoaded()
     {
         if (_meshesLoaded) return;
-        _memberMesh = ModelLoader.LoadObjFromResourceSync("CylinderLow8.obj");
-        _nodeMesh = ModelLoader.LoadObjFromResourceSync("GeoSphereLow.Obj");
+        _memberMesh = await ModelLoader.LoadObjFromResource("CylinderLow8.obj");
+        _nodeMesh = await ModelLoader.LoadObjFromResource("GeoSphereLow.Obj");
         _meshesLoaded = true;
     }
 
-    public override void Build(Entity entity, MeshDataComponent meshData = default)
+    public override async void Build(Entity entity, MeshDataComponent meshData = default)
     {
         entity.Type = EntityType.SceneObject;
 
-        EnsureMeshesLoaded();
+        await EnsureMeshesLoaded();
 
         var min = new Vector3(float.MaxValue);
         var max = new Vector3(float.MinValue);
@@ -85,14 +85,14 @@ public class MemberElementBlueprint : EntityBlueprint
         BuildMember(entity, _memberMesh, _nodeMesh, endA, endB);
     }
 
-    public void BuildAtPositions(Entity entity, Vector3 startPosition, Vector3 endPosition)
+    public async void BuildAtPositions(Entity entity, Vector3 startPosition, Vector3 endPosition)
     {
         entity.Type = EntityType.SceneObject;
-        EnsureMeshesLoaded();
+        await EnsureMeshesLoaded();
         BuildMember(entity, _memberMesh, _nodeMesh, startPosition, endPosition);
     }
 
-    /// <summary>Call only after EnsureMeshesLoaded() has been called.</summary>
+    /// <summary>Call only after EnsureMeshesLoaded() has been awaited.</summary>
     public void BuildMemberSync(Entity entity, Vector3 startPosition, Vector3 endPosition)
     {
         entity.Type = EntityType.SceneObject;
